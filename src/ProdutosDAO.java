36d68231061390f26403a3fec3a72f10b28f6e65
Juliano Barcellos <Juliano28052306@gmail.com>
2023-08-31 16:35:08 -0300

    Botao cadastrar esta apto para uso, alem disso mudanças na classe produtoDAO tais como construtor e implementacao do metodo cadastrarProduto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,44 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    private Connection con;
+    private conectaDAO conexao;
+    private Statement stm = null;
+    private PreparedStatement prep;
+    private ResultSet resultset;
+
+    public ProdutosDAO() {
+        this.conexao = new conectaDAO();
+        this.con = this.conexao.connectDB();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        try {
+           // con = conexao.connectDB();
+            stm = con.createStatement();
+            stm.executeUpdate("INSERT INTO produtos (Nome, Valor, Status) VALUES ('" + produto.getNome() + "', '" + produto.getValor() + "', '" + produto.getStatus() + "')");
+            System.out.println("Dados inseridos.");
+        } catch (SQLException sqle) {
+            System.out.println("Erro inserindo: " + sqle.getMessage());
+
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
