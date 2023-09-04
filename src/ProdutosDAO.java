870d4292f3dd57bf44c05c6cd28357c24c1c98a6
Juliano Barcellos <Juliano28052306@gmail.com>
2023-09-04 10:07:41 -0300

    vender ptoduto id agora é um formatedTextField

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -42,26 +42,48 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Erro inserindo: " + sqle.getMessage());
         }
     }
-
+//
+//    public void venderProduto(ProdutosDTO produto) {
+//        try 
+//        {
+//            stm = con.createStatement();
+//            stm.executeUpdate("update produtos set status='VendidAASDo' where nome ='S');");
+//            JOptionPane.showMessageDialog(null, "Dados inseridos.");
+//        } 
+//        catch (SQLException sqle) 
+//        {
+//            JOptionPane.showMessageDialog(null, "Erro inserindo: " + sqle.getMessage());
+//        }
+//    }
     public void venderProduto(ProdutosDTO produto) {
-        try 
-        {
-            stm = con.createStatement();
-            stm.executeUpdate("update produtos set status='Vendido' where nome ='" + produto.getNome() + "')");
-            JOptionPane.showMessageDialog(null, "Dados inseridos.");
-        } 
-        catch (SQLException sqle) 
-        {
-            JOptionPane.showMessageDialog(null, "Erro inserindo: " + sqle.getMessage());
+    try {
+        stm = con.createStatement();
+        
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setInt(1, produto.getId());
+
+        int rowsAffected = pstmt.executeUpdate();
+
+        if (rowsAffected > 0) {
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Produto não encontrado ou não vendido.");
         }
+    } catch (SQLException sqle) {
+        JOptionPane.showMessageDialog(null, "Erro ao vender o produto: " + sqle.getMessage());
     }
+}
+
 
     public ResultSet listarProdutos() {
         try 
         {
             stm = con.createStatement();
-            return stm.executeQuery("SELECT * FROM produtos where status='Vendido'");
+            return stm.executeQuery("SELECT * FROM produtos ");
         } 
+//        where status='Vendido'
         catch (SQLException ex) 
         {
             Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
